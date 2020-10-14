67f4c7ce5c952c6b7286dae20ebe8a08a1334916
baibigroot <karlygachbekmurzaeva>
2020-10-14 21:31:10 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+//   тут про Буливен операторы
+    public static void main(String[] args) {
+	int age = 16;
+	int temperature = 20;
+	boolean isRainy = false;
+
+	if (!isRainy && temperature < 21){
+        System.out.println("Do not walk!");
+    }
+
+    }
+}
